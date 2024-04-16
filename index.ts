c7bae2e153c54ae99de7beb15a80587e95c6e6ea
MaryamAkberAli <ccamarch202207@gmail.com>
2024-04-16 21:12:02 +0500

    welcome to my todo_list

---
+++ index.ts
@@ -0,0 +1,42 @@
+import inquirer from "inquirer";
+let todos = [];
+let condition = true
+while(condition) 
+    { 
+        let addTask = await inquirer.prompt(
+        [
+            {
+                name:"todo",
+                message:"what you want to add in your Todos",
+                type:"input",
+    
+            } ,{
+                name:"addMore",
+               message:"Do you want to add more?",
+               type:"confirm",
+            }
+        ]
+    );
+    
+    todos.push(addTask.todo);
+    condition = addTask.addMore
+    console.log(todos);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
